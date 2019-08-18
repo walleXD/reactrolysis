aedaefead5de7dd2e95a54d181166c81606b46c4
Walee Ahmed <dev@walee.me>
2019-08-18 02:45:23 -0400

    fixed jest config for project dir

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 const { defaults: tsjPreset } = require('ts-jest/presets')
 
 const common = {
+  roots: ['<rootDir>/src'],
   transform: {
     ...tsjPreset.transform
   },
@@ -23,14 +24,12 @@ module.exports = {
   projects: [
     {
       ...common,
-      roots: ['<rootDir>/src/main'],
       runner: '@jest-runner/electron/main',
       testEnvironment: 'node',
       testMatch: ['**/__tests__/**/*.(spec|test).ts']
     },
     {
       ...common,
-      roots: ['<rootDir>/src/renderer'],
       runner: '@jest-runner/electron',
       testEnvironment: '@jest-runner/electron/environment',
       testMatch: ['**/__tests__/**/*.(spec|test).tsx']
