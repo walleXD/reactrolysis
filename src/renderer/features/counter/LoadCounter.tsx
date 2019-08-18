ba931e70dbbbdbc8e73aa1f10a2c11cac3549f24
Walee Ahmed <dev@walee.me>
2019-08-18 00:12:16 -0400

    cleanup counter

--- src/renderer/features/counter/LoadCounter.tsx
+++ src/renderer/features/counter/LoadCounter.tsx
@@ -1,12 +1,25 @@
 import React, { ReactElement } from 'react'
-import { Button, Typography } from '@material-ui/core'
+import {
+  Button,
+  Typography,
+  makeStyles,
+  Box
+} from '@material-ui/core'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { RootState } from 'AppReduxTypes'
 
 import { counterActions } from '../../../common/features/counter'
 
+const useStyles = makeStyles({
+  btn: {
+    margin: 5
+  }
+})
+
 const LoadCounter = (): ReactElement => {
+  const { btn } = useStyles({})
+
   const dispatch = useDispatch()
 
   const count = useSelector<RootState, number>(
@@ -14,23 +27,33 @@ const LoadCounter = (): ReactElement => {
   )
 
   return (
-    <>
-      <Typography>{count}</Typography>
-      <Button
-        onClick={(): void => {
-          dispatch(counterActions.add())
-        }}
-      >
-        Add
-      </Button>
-      <Button
-        onClick={(): void => {
-          dispatch(counterActions.substract())
-        }}
-      >
-        Substract
-      </Button>
-    </>
+    <Box display="flex" flexDirection="column" m={5}>
+      <Box display="flex" justifyContent="center" p={10}>
+        <Typography variant="h1">{count}</Typography>
+      </Box>
+      <Box display="flex" justifyContent="center">
+        <Button
+          className={btn}
+          variant="contained"
+          color="primary"
+          onClick={(): void => {
+            dispatch(counterActions.add())
+          }}
+        >
+          Add
+        </Button>
+        <Button
+          className={btn}
+          variant="contained"
+          color="secondary"
+          onClick={(): void => {
+            dispatch(counterActions.substract())
+          }}
+        >
+          Substract
+        </Button>
+      </Box>
+    </Box>
   )
 }
 
--- src/renderer/pages/home.tsx
+++ src/renderer/pages/home.tsx
@@ -1,12 +1,10 @@
 import React, { ReactElement } from 'react'
-import { Typography } from '@material-ui/core'
 
 import Layout from '../components/Layout'
 import Counter from '../features/counter/LoadCounter'
 
 const HomePage = (): ReactElement => (
   <Layout>
-    <Typography>Home</Typography>
     <Counter />
   </Layout>
 )
