e41aa3510d5bd2280db2a9130e0fea6b163c99ae
Walee Ahmed <dev@walee.me>
2019-08-18 23:47:55 -0400

    added storybook setup

---
+++ src/modules/settings/AllSettings.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react'
+
+const AllSettings = (): ReactElement => (
+  <h1>Hello Settings</h1>
+)
+
+export default AllSettings
---
+++ src/modules/settings/__stories__/AllSettings.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import AllSettings from '../AllSettings'
+
+storiesOf('All Settings', module).add('basic', () => (
+  <AllSettings />
+))
